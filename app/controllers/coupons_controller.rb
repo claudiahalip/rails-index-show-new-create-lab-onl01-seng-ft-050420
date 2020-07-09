b7f2c34401a26c9b7152c098b02affedd870dc32
claudia v <claudia4ro@yahoo.com>
2020-07-09 20:30:17 +0000

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -9,6 +9,15 @@ class CouponsController < ApplicationController
   end 
   
   def new 
+    
+  end 
+  
+  def create 
+    @coupon = Coupon.new
+    @coupon.coupon_code = params[:coupon][:coupon_code]
+    @coupon.store = params[:coupon][:store]
+    @coupon.save
+    redirect_to coupon_path(@coupon)
   end 
   
 end 
